64267e6d8868fd00878b1cab6817181cbb75a1c2
thefather12 <94747184+thefather12@users.noreply.github.com>
2023-09-24 23:05:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,137 +1,97 @@
-#!/bin/bash
-
-# Run as root
-[[ "$(whoami)" != "root" ]] && {
-    echo -e "\033[1;33m[\033[1;31mErro\033[1;33m] \033[1;37m- \033[1;33myou need to run as root\033[0m"
-    rm /home/ubuntu/install.sh &>/dev/null
-    exit 0
-}
-
-#=== setup ===
-cd 
-rm -rf /root/udp
-mkdir -p /root/udp
-rm -rf /etc/UDPCustom
-mkdir -p /etc/UDPCustom
-sudo touch /etc/UDPCustom/udp-custom
-udp_dir='/etc/UDPCustom'
-udp_file='/etc/UDPCustom/udp-custom'
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y wget
-sudo apt install -y curl
-sudo apt install -y dos2unix
-sudo apt install -y neofetch
-
-source <(curl -sSL 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/module')
-
-time_reboot() {
-  print_center -ama "${a92:-System/Server Reboot In} $1 ${a93:-Seconds}"
-  REBOOT_TIMEOUT="$1"
-
-  while [ $REBOOT_TIMEOUT -gt 0 ]; do
-    print_center -ne "-$REBOOT_TIMEOUT-\r"
-    sleep 1
-    : $((REBOOT_TIMEOUT--))
-  done
-  rm /home/ubuntu/install.sh &>/dev/null
-  rm /root/install.sh &>/dev/null
-  echo -e "\033[01;31m\033[1;33m More Updates, Follow Us On \033[1;31m(\033[1;36mTelegram\033[1;31m): \033[1;37m@voltssh\033[0m"
-  reboot
-}
-
-# Check Ubuntu version
-if [ "$(lsb_release -rs)" = "8*|9*|10*|11*|16.04*|18.04*" ]; then
-  clear
-  print_center -ama -e "\e[1m\e[31m=====================================================\e[0m"
-  print_center -ama -e "\e[1m\e[33m${a94:-this script is not compatible with your operating system}\e[0m"
-  print_center -ama -e "\e[1m\e[33m ${a95:-Use Ubuntu 20 or higher}\e[0m"
-  print_center -ama -e "\e[1m\e[31m=====================================================\e[0m"
-  rm /home/ubuntu/install.sh
-  exit 1
-else
-  clear
-  echo ""
-  print_center -ama "A Compatible OS/Environment Found"
-  print_center -ama " ⇢ Installation begins...! <"
-  sleep 3
-
-    # [change timezone to UTC +0]
-  echo ""
-  echo " ⇢ Change timezone to UTC +0"
-  echo " ⇢ for Africa/Accra [GH] GMT +00:00"
-  ln -fs /usr/share/zoneinfo/Africa/Accra /etc/localtime
-  sleep 3
-
-  # [+clean up+]
-  rm -rf $udp_file &>/dev/null
-  rm -rf /etc/UDPCustom/udp-custom &>/dev/null
-  # rm -rf /usr/bin/udp-request &>/dev/null
-  rm -rf /etc/limiter.sh &>/dev/null
-  rm -rf /etc/UDPCustom/limiter.sh &>/dev/null
-  rm -rf /etc/UDPCustom/module &>/dev/null
-  rm -rf /usr/bin/udp &>/dev/null
-  rm -rf /etc/UDPCustom/udpgw.service &>/dev/null
-  rm -rf /etc/udpgw.service &>/dev/null
-  systemctl stop udpgw &>/dev/null
-  systemctl stop udp-custom &>/dev/null
-  # systemctl stop udp-request &>/dev/null
-
- # [+get files ⇣⇣⇣+]
-  source <(curl -sSL 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/module') &>/dev/null
-  wget -O /etc/UDPCustom/module 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/module' &>/dev/null
-  chmod +x /etc/UDPCustom/module
-
-  wget "https://raw.githubusercontent.com/thefather12/UDP-PRO/main/bin/udp-custom-linux-amd64" -O /root/udp/udp-custom &>/dev/null
-  # wget "https://raw.githubusercontent.com/prjkt-nv404/UDP-Custom-Installer-Manager/main/bin/udp-request-linux-amd64" -O /usr/bin/udp-request &>/dev/null
-  chmod +x /root/udp/udp-custom
-  # chmod +x /usr/bin/udp-request
-
-  wget -O /etc/limiter.sh 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/limiter.sh'
-  cp /etc/limiter.sh /etc/UDPCustom
-  chmod +x /etc/limiter.sh
-  chmod +x /etc/UDPCustom
-  
-  # [+udpgw+]
-  wget -O /etc/udpgw 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/udpgw'
-  mv /etc/udpgw /bin
-  chmod +x /bin/udpgw
-
-  # [+service+]
-  wget -O /etc/udpgw.service 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/config/udpgw.service'
-  wget -O /etc/udp-custom.service 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/config/udp-custom.service'
-  
-  mv /etc/udpgw.service /etc/systemd/system
-  mv /etc/udp-custom.service /etc/systemd/system
-
-  chmod 640 /etc/systemd/system/udpgw.service
-  chmod 640 /etc/systemd/system/udp-custom.service
-  
-  systemctl daemon-reload &>/dev/null
-  systemctl enable udpgw &>/dev/null
-  systemctl start udpgw &>/dev/null
-  systemctl enable udp-custom &>/dev/null
-  systemctl start udp-custom &>/dev/null
-
-  # [+config+]
-  wget "https://raw.githubusercontent.com/thefather12/UDP-PRO/main/config/config.json" -O /root/udp/config.json &>/dev/null
-  chmod +x /root/udp/config.json
-
-  # [+menu+]
-  wget -O /usr/bin/udp 'https://raw.githubusercontent.com/thefather12/UDP-PRO/main/module/udp' 
-  chmod +x /usr/bin/udp
-  ufw disable &>/dev/null
-  sudo apt-get remove --purge ufw firewalld -y
-  apt remove netfilter-persistent -y
-  clear
-  echo ""
-  echo ""
-  print_center -ama "${a103:-setting up, please wait...}"
-  sleep 6
-  title "${a102:-Installation Successful}"
-  print_center -ama "${a103:-  To show menu type: \nudp\n}"
-  echo -ne "\n\033[1;31mENTER \033[1;33mpara entrar al \033[1;32mMENU!\033[0m"; read
-   udp
-  
-fi
+#!/usr/bin/env bash
+#
+# Obfuscate by @CesarHackGray
+# Contacta https://t.me/CesarGray
+# Channel: https://www.youtube.com/channel/UCjs0N8PbEo-se0r_4O_svNQ
+#
+#
+CesarHackGray=$(mktemp)
+base64 -d  >${CesarHackGray}<<DIXIE
+IyEvYmluL2Jhc2gKCiMgUnVuIGFzIHJvb3QKW1sgIiQod2hvYW1pKSIgIT0gInJvb3QiIF1dICYm
+IHsKICAgIGVjaG8gLWUgIlwwMzNbMTszM21bXDAzM1sxOzMxbUVycm9cMDMzWzE7MzNtXSBcMDMz
+WzE7MzdtLSBcMDMzWzE7MzNteW91IG5lZWQgdG8gcnVuIGFzIHJvb3RcMDMzWzBtIgogICAgcm0g
+L2hvbWUvdWJ1bnR1L2luc3RhbGwuc2ggJj4vZGV2L251bGwKICAgIGV4aXQgMAp9CgojPT09IHNl
+dHVwID09PQpjZCAKcm0gLXJmIC9yb290L3VkcApta2RpciAtcCAvcm9vdC91ZHAKcm0gLXJmIC9l
+dGMvVURQQ3VzdG9tCm1rZGlyIC1wIC9ldGMvVURQQ3VzdG9tCnN1ZG8gdG91Y2ggL2V0Yy9VRFBD
+dXN0b20vdWRwLWN1c3RvbQp1ZHBfZGlyPScvZXRjL1VEUEN1c3RvbScKdWRwX2ZpbGU9Jy9ldGMv
+VURQQ3VzdG9tL3VkcC1jdXN0b20nCgpzdWRvIGFwdCB1cGRhdGUgLXkKc3VkbyBhcHQgdXBncmFk
+ZSAteQpzdWRvIGFwdCBpbnN0YWxsIC15IHdnZXQKc3VkbyBhcHQgaW5zdGFsbCAteSBjdXJsCnN1
+ZG8gYXB0IGluc3RhbGwgLXkgZG9zMnVuaXgKc3VkbyBhcHQgaW5zdGFsbCAteSBuZW9mZXRjaAoK
+c291cmNlIDwoY3VybCAtc1NMICdodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vdGhl
+ZmF0aGVyMTIvVURQLVBSTy9tYWluL21vZHVsZS9tb2R1bGUnKQoKdGltZV9yZWJvb3QoKSB7CiAg
+cHJpbnRfY2VudGVyIC1hbWEgIiR7YTkyOi1TeXN0ZW0vU2VydmVyIFJlYm9vdCBJbn0gJDEgJHth
+OTM6LVNlY29uZHN9IgogIFJFQk9PVF9USU1FT1VUPSIkMSIKCiAgd2hpbGUgWyAkUkVCT09UX1RJ
+TUVPVVQgLWd0IDAgXTsgZG8KICAgIHByaW50X2NlbnRlciAtbmUgIi0kUkVCT09UX1RJTUVPVVQt
+XHIiCiAgICBzbGVlcCAxCiAgICA6ICQoKFJFQk9PVF9USU1FT1VULS0pKQogIGRvbmUKICBybSAv
+aG9tZS91YnVudHUvaW5zdGFsbC5zaCAmPi9kZXYvbnVsbAogIHJtIC9yb290L2luc3RhbGwuc2gg
+Jj4vZGV2L251bGwKICBlY2hvIC1lICJcMDMzWzAxOzMxbVwwMzNbMTszM20gTW9yZSBVcGRhdGVz
+LCBGb2xsb3cgVXMgT24gXDAzM1sxOzMxbShcMDMzWzE7MzZtVGVsZWdyYW1cMDMzWzE7MzFtKTog
+XDAzM1sxOzM3bUB2b2x0c3NoXDAzM1swbSIKICByZWJvb3QKfQoKIyBDaGVjayBVYnVudHUgdmVy
+c2lvbgppZiBbICIkKGxzYl9yZWxlYXNlIC1ycykiID0gIjgqfDkqfDEwKnwxMSp8MTYuMDQqfDE4
+LjA0KiIgXTsgdGhlbgogIGNsZWFyCiAgcHJpbnRfY2VudGVyIC1hbWEgLWUgIlxlWzFtXGVbMzFt
+PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT1cZVsw
+bSIKICBwcmludF9jZW50ZXIgLWFtYSAtZSAiXGVbMW1cZVszM20ke2E5NDotdGhpcyBzY3JpcHQg
+aXMgbm90IGNvbXBhdGlibGUgd2l0aCB5b3VyIG9wZXJhdGluZyBzeXN0ZW19XGVbMG0iCiAgcHJp
+bnRfY2VudGVyIC1hbWEgLWUgIlxlWzFtXGVbMzNtICR7YTk1Oi1Vc2UgVWJ1bnR1IDIwIG9yIGhp
+Z2hlcn1cZVswbSIKICBwcmludF9jZW50ZXIgLWFtYSAtZSAiXGVbMW1cZVszMW09PT09PT09PT09
+PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PVxlWzBtIgogIHJtIC9o
+b21lL3VidW50dS9pbnN0YWxsLnNoCiAgZXhpdCAxCmVsc2UKICBjbGVhcgogIGVjaG8gIiIKICBw
+cmludF9jZW50ZXIgLWFtYSAiQSBDb21wYXRpYmxlIE9TL0Vudmlyb25tZW50IEZvdW5kIgogIHBy
+aW50X2NlbnRlciAtYW1hICIg4oeiIEluc3RhbGxhdGlvbiBiZWdpbnMuLi4hIDwiCiAgc2xlZXAg
+MwoKICAgICMgW2NoYW5nZSB0aW1lem9uZSB0byBVVEMgKzBdCiAgZWNobyAiIgogIGVjaG8gIiDi
+h6IgQ2hhbmdlIHRpbWV6b25lIHRvIFVUQyArMCIKICBlY2hvICIg4oeiIGZvciBBZnJpY2EvQWNj
+cmEgW0dIXSBHTVQgKzAwOjAwIgogIGxuIC1mcyAvdXNyL3NoYXJlL3pvbmVpbmZvL0FmcmljYS9B
+Y2NyYSAvZXRjL2xvY2FsdGltZQogIHNsZWVwIDMKCiAgIyBbK2NsZWFuIHVwK10KICBybSAtcmYg
+JHVkcF9maWxlICY+L2Rldi9udWxsCiAgcm0gLXJmIC9ldGMvVURQQ3VzdG9tL3VkcC1jdXN0b20g
+Jj4vZGV2L251bGwKICAjIHJtIC1yZiAvdXNyL2Jpbi91ZHAtcmVxdWVzdCAmPi9kZXYvbnVsbAog
+IHJtIC1yZiAvZXRjL2xpbWl0ZXIuc2ggJj4vZGV2L251bGwKICBybSAtcmYgL2V0Yy9VRFBDdXN0
+b20vbGltaXRlci5zaCAmPi9kZXYvbnVsbAogIHJtIC1yZiAvZXRjL1VEUEN1c3RvbS9tb2R1bGUg
+Jj4vZGV2L251bGwKICBybSAtcmYgL3Vzci9iaW4vdWRwICY+L2Rldi9udWxsCiAgcm0gLXJmIC9l
+dGMvVURQQ3VzdG9tL3VkcGd3LnNlcnZpY2UgJj4vZGV2L251bGwKICBybSAtcmYgL2V0Yy91ZHBn
+dy5zZXJ2aWNlICY+L2Rldi9udWxsCiAgc3lzdGVtY3RsIHN0b3AgdWRwZ3cgJj4vZGV2L251bGwK
+ICBzeXN0ZW1jdGwgc3RvcCB1ZHAtY3VzdG9tICY+L2Rldi9udWxsCiAgIyBzeXN0ZW1jdGwgc3Rv
+cCB1ZHAtcmVxdWVzdCAmPi9kZXYvbnVsbAoKICMgWytnZXQgZmlsZXMg4oej4oej4oejK10KICBz
+b3VyY2UgPChjdXJsIC1zU0wgJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS90aGVm
+YXRoZXIxMi9VRFAtUFJPL21haW4vbW9kdWxlL21vZHVsZScpICY+L2Rldi9udWxsCiAgd2dldCAt
+TyAvZXRjL1VEUEN1c3RvbS9tb2R1bGUgJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNv
+bS90aGVmYXRoZXIxMi9VRFAtUFJPL21haW4vbW9kdWxlL21vZHVsZScgJj4vZGV2L251bGwKICBj
+aG1vZCAreCAvZXRjL1VEUEN1c3RvbS9tb2R1bGUKCiAgd2dldCAiaHR0cHM6Ly9yYXcuZ2l0aHVi
+dXNlcmNvbnRlbnQuY29tL3RoZWZhdGhlcjEyL1VEUC1QUk8vbWFpbi9iaW4vdWRwLWN1c3RvbS1s
+aW51eC1hbWQ2NCIgLU8gL3Jvb3QvdWRwL3VkcC1jdXN0b20gJj4vZGV2L251bGwKICAjIHdnZXQg
+Imh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9wcmprdC1udjQwNC9VRFAtQ3VzdG9t
+LUluc3RhbGxlci1NYW5hZ2VyL21haW4vYmluL3VkcC1yZXF1ZXN0LWxpbnV4LWFtZDY0IiAtTyAv
+dXNyL2Jpbi91ZHAtcmVxdWVzdCAmPi9kZXYvbnVsbAogIGNobW9kICt4IC9yb290L3VkcC91ZHAt
+Y3VzdG9tCiAgIyBjaG1vZCAreCAvdXNyL2Jpbi91ZHAtcmVxdWVzdAoKICB3Z2V0IC1PIC9ldGMv
+bGltaXRlci5zaCAnaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RoZWZhdGhlcjEy
+L1VEUC1QUk8vbWFpbi9tb2R1bGUvbGltaXRlci5zaCcKICBjcCAvZXRjL2xpbWl0ZXIuc2ggL2V0
+Yy9VRFBDdXN0b20KICBjaG1vZCAreCAvZXRjL2xpbWl0ZXIuc2gKICBjaG1vZCAreCAvZXRjL1VE
+UEN1c3RvbQogIAogICMgWyt1ZHBndytdCiAgd2dldCAtTyAvZXRjL3VkcGd3ICdodHRwczovL3Jh
+dy5naXRodWJ1c2VyY29udGVudC5jb20vdGhlZmF0aGVyMTIvVURQLVBSTy9tYWluL21vZHVsZS91
+ZHBndycKICBtdiAvZXRjL3VkcGd3IC9iaW4KICBjaG1vZCAreCAvYmluL3VkcGd3CgogICMgWytz
+ZXJ2aWNlK10KICB3Z2V0IC1PIC9ldGMvdWRwZ3cuc2VydmljZSAnaHR0cHM6Ly9yYXcuZ2l0aHVi
+dXNlcmNvbnRlbnQuY29tL3RoZWZhdGhlcjEyL1VEUC1QUk8vbWFpbi9jb25maWcvdWRwZ3cuc2Vy
+dmljZScKICB3Z2V0IC1PIC9ldGMvdWRwLWN1c3RvbS5zZXJ2aWNlICdodHRwczovL3Jhdy5naXRo
+dWJ1c2VyY29udGVudC5jb20vdGhlZmF0aGVyMTIvVURQLVBSTy9tYWluL2NvbmZpZy91ZHAtY3Vz
+dG9tLnNlcnZpY2UnCiAgCiAgbXYgL2V0Yy91ZHBndy5zZXJ2aWNlIC9ldGMvc3lzdGVtZC9zeXN0
+ZW0KICBtdiAvZXRjL3VkcC1jdXN0b20uc2VydmljZSAvZXRjL3N5c3RlbWQvc3lzdGVtCgogIGNo
+bW9kIDY0MCAvZXRjL3N5c3RlbWQvc3lzdGVtL3VkcGd3LnNlcnZpY2UKICBjaG1vZCA2NDAgL2V0
+Yy9zeXN0ZW1kL3N5c3RlbS91ZHAtY3VzdG9tLnNlcnZpY2UKICAKICBzeXN0ZW1jdGwgZGFlbW9u
+LXJlbG9hZCAmPi9kZXYvbnVsbAogIHN5c3RlbWN0bCBlbmFibGUgdWRwZ3cgJj4vZGV2L251bGwK
+ICBzeXN0ZW1jdGwgc3RhcnQgdWRwZ3cgJj4vZGV2L251bGwKICBzeXN0ZW1jdGwgZW5hYmxlIHVk
+cC1jdXN0b20gJj4vZGV2L251bGwKICBzeXN0ZW1jdGwgc3RhcnQgdWRwLWN1c3RvbSAmPi9kZXYv
+bnVsbAoKICAjIFsrY29uZmlnK10KICB3Z2V0ICJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVu
+dC5jb20vdGhlZmF0aGVyMTIvVURQLVBSTy9tYWluL2NvbmZpZy9jb25maWcuanNvbiIgLU8gL3Jv
+b3QvdWRwL2NvbmZpZy5qc29uICY+L2Rldi9udWxsCiAgY2htb2QgK3ggL3Jvb3QvdWRwL2NvbmZp
+Zy5qc29uCgogICMgWyttZW51K10KICB3Z2V0IC1PIC91c3IvYmluL3VkcCAnaHR0cHM6Ly9yYXcu
+Z2l0aHVidXNlcmNvbnRlbnQuY29tL3RoZWZhdGhlcjEyL1VEUC1QUk8vbWFpbi9tb2R1bGUvdWRw
+JyAKICBjaG1vZCAreCAvdXNyL2Jpbi91ZHAKICB1ZncgZGlzYWJsZSAmPi9kZXYvbnVsbAogIHN1
+ZG8gYXB0LWdldCByZW1vdmUgLS1wdXJnZSB1ZncgZmlyZXdhbGxkIC15CiAgYXB0IHJlbW92ZSBu
+ZXRmaWx0ZXItcGVyc2lzdGVudCAteQogIGNsZWFyCiAgZWNobyAiIgogIGVjaG8gIiIKICBwcmlu
+dF9jZW50ZXIgLWFtYSAiJHthMTAzOi1zZXR0aW5nIHVwLCBwbGVhc2Ugd2FpdC4uLn0iCiAgc2xl
+ZXAgNgogIHRpdGxlICIke2ExMDI6LUluc3RhbGxhdGlvbiBTdWNjZXNzZnVsfSIKICBwcmludF9j
+ZW50ZXIgLWFtYSAiJHthMTAzOi0gIFRvIHNob3cgbWVudSB0eXBlOiBcbnVkcFxufSIKICBlY2hv
+IC1uZSAiXG5cMDMzWzE7MzFtRU5URVIgXDAzM1sxOzMzbXBhcmEgZW50cmFyIGFsIFwwMzNbMTsz
+Mm1NRU5VIVwwMzNbMG0iOyByZWFkCiAgIHVkcAogIApmaQo=
+DIXIE
+source ${CesarHackGray}
+rm -rf ${CesarHackGray}
